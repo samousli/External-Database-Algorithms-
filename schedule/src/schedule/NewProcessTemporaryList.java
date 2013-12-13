4990bb8e84ad38ebdffe357279b3413884120685
Sachin Mousli <sahin.mousli@gmail.com>
2013-12-14 00:40:43 +0200

    Hello world!

---
+++ schedule/src/schedule/NewProcessTemporaryList.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+import java.util.List;
+
+/**
+ *
+ * @author avail
+ */
+public class NewProcessTemporaryList {
+
+    private List processList;
+
+    public NewProcessTemporaryList() {
+    }
+
+    public void addNewProcess(Process process) {
+    }
+
+    public Process getFirst() {
+        return null;
+    }
+
+    public void printList() {
+    }
+
+}

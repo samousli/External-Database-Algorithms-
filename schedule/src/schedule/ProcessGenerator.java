8548f1292fc9518cdc100d14a567ae6c1c744f8a
Sachin Mousli <sahin.mousli@gmail.com>
2013-12-15 16:07:44 +0200

    ShamWooohooo

--- schedule/src/schedule/ProcessGenerator.java
+++ schedule/src/schedule/ProcessGenerator.java
@@ -1,12 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package schedule;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -17,6 +20,32 @@ public class ProcessGenerator {
     private File inputFile;
 
     public ProcessGenerator(String filename, boolean readFile) {
+        Path p = Paths.get(filename);
+        if (Files.exists((p))) {
+            this.inputFile = new File(filename);
+
+            try {
+                //Initializing the IO
+                BufferedReader input = new BufferedReader(new FileReader(filename));
+                //Reading all the data.
+                String text = input.readLine();
+                //Closing input file.
+                input.close();
+                //Initializing the StringTokenizer which will be used to parse the input.
+                StringTokenizer tokenizer = new StringTokenizer(text);
+
+                //storing the numbers in an array list instead of a simple array 
+                //because its size is unknown during initializion.
+                ArrayList<Integer> temp = new ArrayList<>();
+                //Parsing to array.
+                while (tokenizer.hasMoreTokens()) {
+                    temp.add(Integer.valueOf(Integer.parseInt(tokenizer.nextToken())));
+                }
+            } catch (IOException e) {
+                System.err.println("Error: " + e);
+            }
+        }
+
     }
 
     public Process createProcess() {

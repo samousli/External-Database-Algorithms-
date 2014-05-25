70aff8e13a3cabb9c288c6495c96b362c3b64db3
haristos1991 <geohararnea16@hotmail.com>
2014-05-26 00:22:56 +0300

    Mikri Allagi

--- HelpingMethods.h
+++ HelpingMethods.h
@@ -9,7 +9,7 @@
 #define	HELPINGMETHODS_H
 #include "dbtproj.h"
 #include <iostream>
-
+#include <fstream>
 typedef unsigned int uint;
 
 void SortRecords(block_t *buffer,unsigned int nmem_blocks,char *outputPath,unsigned char field);
@@ -17,8 +17,8 @@ std::pair<int,int> FindMinimumValue(block_t *buffer,unsigned int nmem_blocks,int
 int compare (const void * a, const void * b);
 // Keeping streams open may boost performance but doing this for now to simplify things.
 
-void Write_Block(char* filename, block_t &block);
-void Serialize_Record(char *filename, block_t &block, record_t record);
+void Write_Block(std::ofstream& outfile, block_t block);
+void Serialize_Record(std::ofstream& outfile, block_t &block, record_t record);
 
 
     
